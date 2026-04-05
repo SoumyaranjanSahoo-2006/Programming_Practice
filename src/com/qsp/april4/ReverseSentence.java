88de078057c2dd8450be6722519660559c9a18b4
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-04-05 21:09:50 +0530

    Added PossibleSubstring

--- src/com/qsp/april4/ReverseSentence.java
+++ src/com/qsp/april4/ReverseSentence.java
@@ -57,3 +57,14 @@ class ReveseWordOfSentence{
 
 //possible substring
 
+
+class PossibleSubstring{
+	public static void main(String[] args) {
+		String s="malayalam";
+		for(int i=0;i<s.length();i++) {
+			for(int j=i+1;j<=s.length();j++) {
+				System.out.println(s.substring(i,j));
+			}
+		}
+	}
+}
