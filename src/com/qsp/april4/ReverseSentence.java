01bd8f7953ddc2e40bb5beae51a658703bd88e18
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-04-06 10:59:28 +0530

    add AllPossibleSubstringPalindrome

--- src/com/qsp/april4/ReverseSentence.java
+++ src/com/qsp/april4/ReverseSentence.java
@@ -68,3 +68,30 @@ class PossibleSubstring{
 		}
 	}
 }
+
+class AllPossibleSubstringPalindrome{
+	public static void main(String[] args) {
+		String s="malayalam";
+		for(int i=0;i<s.length();i++) {
+			for(int j=i+1;j<=s.length();j++) {
+				String temp=s.substring(i,j);
+				if(isPalindrome(temp)) {
+					System.out.println(temp);
+				}
+			}
+		}
+	}
+	
+	static boolean isPalindrome(String s) {
+		int i=0;
+		int j=s.length()-1;
+		while(i<j) {
+			if(s.charAt(i)!=s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+}
