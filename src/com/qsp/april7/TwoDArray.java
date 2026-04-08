9163bcf68cbdd11b27c0971fb8df40a391795018
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-04-08 11:13:43 +0530

    added 2D array

--- src/com/qsp/april7/TwoDArray.java
+++ src/com/qsp/april7/TwoDArray.java
@@ -1,5 +1,7 @@
 package com.qsp.april7;
 
+import java.util.Arrays;
+
 public class TwoDArray {
 	public static void main(String[] args) {
 		int a[][]= {
@@ -19,6 +21,26 @@ public class TwoDArray {
 				System.out.print(a[i][j]+" ");
 			}
 			System.out.println();
+		}
+	}
+}
+
+
+class TwoDArray2{
+	public static void main(String[] args) {
+		int n=5;
+		int a[][]= new int[n][];
+		for(int i=0;i<a.length;i++) {
+			a[i]=new int[i+1];
+			for(int j=0;j<a[i].length;j++) {
+				if(j==0 || j==a[i].length-1) {
+					a[i][j]=1;
+				}
+			}
+		}
+		
+		for(int x[]:a) {
+			System.out.println(Arrays.toString(x));
 		}
 	}
 }
