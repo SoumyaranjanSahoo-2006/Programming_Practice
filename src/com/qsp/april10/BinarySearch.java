6108ff2474bdbcf68766b94cfe9b603b05ef41d7
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-04-10 11:26:42 +0530

    added BinarySearch

---
+++ src/com/qsp/april10/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.qsp.april10;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+		int a[]= {1,3,4,5,7,8,10,12};
+		System.out.println(solution(a, 9));
+	}
+	static int solution(int a[], int key) {
+		int start=0;
+		int end=a.length-1;
+		while(start<=end) {
+			int mid=(start+end)/2;
+			if(a[mid]==key) {
+				return mid;
+			}
+			else if(a[mid]<key) {
+				start=mid+1;
+			}
+			else {
+				end=mid-1;
+			}
+		}
+		return -1;
+	}
+}
