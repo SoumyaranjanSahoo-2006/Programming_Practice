0f825bda4c1382e121c958beed3be6c8b1a3313c
Soumyaranjan Sahoo <soumyaranjansahu3566@gmail.com>
2026-04-10 12:05:23 +0530

    added SecondMax

--- src/com/qsp/april10/BinarySearch.java
+++ src/com/qsp/april10/BinarySearch.java
@@ -64,3 +64,25 @@ class SecondMax{
 		return secondmax;
 	}
 }
+
+
+class SecondMax2{
+	public static void main(String[] args) {
+		int a[]= {2,3,5,1,6,2,7,9,8,9};
+		System.out.println(solution(a));
+	}
+	static int solution(int a[]) {
+		int max=Integer.MIN_VALUE;
+		int secondmax=Integer.MIN_VALUE;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]>max) {
+				secondmax=max;
+				max=a[i];
+			}
+			else if(a[i]>secondmax && a[i]!=max) {
+				secondmax=a[i];
+			}
+		}
+		return secondmax;
+	}
+}
